3ceb699b6680d9367692741595f04952c57ccd88
zakrza59 <zakrza59@gmail.com>
2018-10-29 13:02:50 +0100

    firebase picture upload works

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,57 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
+const fs = require("fs");
+const UUID = require("uuid-v4");
+
+const gcconfig = {
+  projectId: "awesome-places-1540584656353",
+  keyFilename: "awesome-places.json"
+};
+
+const { Storage } = require("@google-cloud/storage");
+const gcs = new Storage(gcconfig);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.storeImage = functions.https.onRequest((request, response) => {
+  return cors(request, response, () => {
+    const body = JSON.parse(request.body);
+    fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", err => {
+      console.log(err);
+      return response.status(500).json({ error: err });
+    });
+    const bucket = gcs.bucket("awesome-places-1540584656353.appspot.com");
+    const uuid = UUID();
+
+    return bucket.upload(
+      "/tmp/uploaded-image.jpg",
+      {
+        uploadType: "media",
+        destination: "/places/" + uuid + ".jpg",
+        metadata: {
+          metadata: {
+            contentType: "image/jpeg",
+            firebaseStorageDownloadTokens: uuid
+          }
+        }
+      },
+      (err, file) => {
+        if (!err) {
+          return response.status(201).json({
+            imageUrl:
+              "https://firebasestorage.googleapis.com/v0/b/" +
+              bucket.name +
+              "/o/" +
+              encodeURIComponent(file.name) +
+              "?alt=media&token=" +
+              uuid
+          });
+        } else {
+          console.log(err);
+          return response.status(500).json({ error: err });
+        }
+      }
+    );
+  });
+});
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,26 +1,77 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-    return dispatch => {
-      const placeData = {
-        name: placeName,
-        location: location
-      };
-      fetch("https://awesome-places-1540584656353.firebaseio.com/places.json", {
-        method: "POST",
-        body: JSON.stringify(placeData)
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location,
+      // image: parsedRes.imageUrl
+    };
+    fetch("https://us-central1-awesome-places-1540584656353.cloudfunctions.net/storeImage", {
+      method: "POST",
+      body: JSON.stringify({
+        image: image.base64
       })
+    })
       .catch(err => console.log(err))
       .then(res => res.json())
       .then(parsedRes => {
-        console.log(parsedRes);
+        console.log(parsedRes)
       })
-    }
+      //   return fetch("https://awesome-places-1540584656353.firebaseio.com/places.json", {
+      //     method: "POST",
+      //     body: JSON.stringify(placeData)
+      //   })
+      // })
+      // .catch(err => console.log(err))
+      // .then(res => res.json())
+      // .then(parsedRes => {
+      //   console.log(parsedRes);
+      // });
+  };
 };
 
 export const deletePlace = (key) => {
-    return {
-        type: DELETE_PLACE,
-        placeKey: key
-    };
+  return {
+    type: DELETE_PLACE,
+    placeKey: key
+  };
 };
+
+// import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
+
+// export const addPlace = (placeName, location, image) => {
+//     return dispatch => {
+//         fetch("https://us-central1-awesome-places-1540584656353.cloudfunctions.net/storeImage", {
+//             method: "POST",
+//             body: JSON.stringify({
+//                 image: image.base64
+//             })
+//         })
+//         .catch(err => console.log(err))
+//         .then(res => res.json())
+//         .then(parsedRes => {
+//             const placeData = {
+//                 name: placeName,
+//                 location: location,
+//                 image: parsedRes.imageUrl
+//             };
+//             return fetch("https://awesome-places-1540584656353.firebaseio.com/places.json", {
+//                 method: "POST",
+//                 body: JSON.stringify(placeData)
+//             })
+//         })  
+//         .catch(err => console.log(err))
+//         .then(res => res.json())
+//         .then(parsedRes => {
+//             console.log(parsedRes);
+//         });
+//     };
+// };
+
+// export const deletePlace = (key) => {
+//     return {
+//         type: DELETE_PLACE,
+//         placeKey: key
+//     };
+// };
