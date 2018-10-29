3980f98a971a7bddb0b98b3a1ae11b03fa803781
zakrza59 <zakrza59@gmail.com>
2018-10-29 10:30:07 +0100

    added firebase conf

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,12 +1,21 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-    return {
-        type: ADD_PLACE,
-        placeName: placeName,
-        location: location,
-        image: image
-    };
+    return dispatch => {
+      const placeData = {
+        name: placeName,
+        location: location
+      };
+      fetch("https://awesome-places-1540584656353.firebaseio.com/places.json", {
+        method: "POST",
+        body: JSON.stringify(placeData)
+      })
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      })
+    }
 };
 
 export const deletePlace = (key) => {
